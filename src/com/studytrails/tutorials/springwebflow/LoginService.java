562e5864172c50648400466622186cf536dbb90d
Chengguo_Wu <wucg@litsoft.com.cn>
2015-06-24 15:32:07 +0800

    init project

---
+++ src/com/studytrails/tutorials/springwebflow/LoginService.java
@@ -0,0 +1,20 @@
+package com.studytrails.tutorials.springwebflow;
+
+public class LoginService {
+
+	public String performLogin(LoginCredentials loginCredentials)
+			throws IncorrectLoginCredentialsException {
+
+		if (loginCredentials.getLoginName() != null
+				&& loginCredentials.getLoginName().trim()
+						.equalsIgnoreCase("wwu")
+				&& loginCredentials.getPassword() != null
+				&& loginCredentials.getPassword().trim()
+						.equalsIgnoreCase("pass")) {
+			// user successfully logged in
+			return "success";
+		} else {
+			throw new IncorrectLoginCredentialsException();
+		}
+	}
+}
